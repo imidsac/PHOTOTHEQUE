0aad6976118f8dab1ea0ca700c4567d4ee998a41
imidsac <imidsac@hotmail.fr>
2014-10-24 16:14:08 +0000

    Configuration de devise pour qu'il puise connecter avec username.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024072139) do
+ActiveRecord::Schema.define(version: 20141024112645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -227,6 +227,7 @@ ActiveRecord::Schema.define(version: 20141024072139) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
