2e68b16cd8bddd7b30d0b1915dc64f396d1517d3
imidsac <imidsac@hotmail.fr>
2014-10-24 20:09:01 +0000

    mise en formes les differents fichiers de chaque table pour la 2eme.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	#after_sign_in_path_for(:sign_in)
 	before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -11,5 +12,6 @@ class ApplicationController < ActionController::Base
   	#devise_parameter_sanitizer.for(:sign_up) << :username
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :username, :email, :password, :password_confirmation) }
+  
   end
 end
