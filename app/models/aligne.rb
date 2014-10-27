b3e2ea0b23442fabbe2bcd663a19e03dfeddce50
imidsac <imidsac@hotmail.fr>
2014-10-27 00:15:56 +0000

    calcul de montant dans model aligne.rb.

--- app/models/aligne.rb
+++ app/models/aligne.rb
@@ -4,11 +4,17 @@ class Aligne < ActiveRecord::Base
   belongs_to :cadre
   validates :achat_id, presence: true
 
-  after_create :give_montant_to_somme
+  before_save :calcul_de_montant
+  after_save :give_montant_to_somme
   after_destroy :givey_montant_to_somme
 
+
+  def calcul_de_montant
+  	self.montant = qte * prix_u
+  end
+
   def give_montant_to_somme
-  	achat.somme += montant 
+  	achat.somme = montant 
   	achat.save
   end
 
