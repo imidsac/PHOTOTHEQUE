9eef0b3ba92a3944d1c40242158d2666d9d52f87
imidsac <imidsac@hotmail.fr>
2015-01-29 22:53:09 +0000

    Supression deux lignes.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,4 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  config.gem "cancan"
-  config.gem "prawn"
 end
