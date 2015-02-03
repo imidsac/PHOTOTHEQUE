7e7f26ce2a9a97a32a03c17aeca00566b79a1ed4
imidsac <imidsac@hotmail.fr>
2015-02-03 14:07:31 +0000

    Correction de certains errors dans prestations et plignes.

--- app/models/prestationligne.rb
+++ app/models/prestationligne.rb
@@ -3,14 +3,29 @@ class Prestationligne < ActiveRecord::Base
   belongs_to :cadre
   belongs_to :formatphoto
 
-  before_save :montant 
+  validates :prestation_id, presence: true#, scope: [:formatphoto_id, :cadre_id]
+
+  validates_uniqueness_of :prestation_id, scope: [:formatphoto_id, :cadre_id]
+
+  validates_numericality_of :qtelivre, :qte
+  validates_presence_of :qtelivre, :qte, :prix_u
+
+  #jointure avec formatphoto et cadre
+  scope :formatphoto, -> { joins(:formatphoto).where("formatphoto_id != -1").order(:created_at) }
+  scope :cadre, -> { joins(:cadre).where("cadre_id != -1").order(:created_at) }
+
+  before_save :verifier_vligne
   after_save  :total
   #after_save :total
   after_destroy :total
 
 
-  def montant
+  def verifier_vligne
     self.montant = qte * prix_u
+    if qtelivre > qte
+      return false
+    end
+    #self.save
   end
 
   def total
