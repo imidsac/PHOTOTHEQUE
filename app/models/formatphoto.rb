63f6d83aa154c43a0aec0b5e15195bc03a677d61
imidsac <imidsac@hotmail.fr>
2015-02-03 14:08:39 +0000

    scope de list dans formatphotos.

--- app/models/formatphoto.rb
+++ app/models/formatphoto.rb
@@ -2,4 +2,5 @@ class Formatphoto < ActiveRecord::Base
 	#default_scope { where("id != ? ", -1)}
 	default_scope { order(:dimention)}
 	has_many :prestationlignes
+	scope :list, -> { where("formatphotos.id != ? ", -1) }
 end
